b11b2fb8c7172b5de2ca128a4d372d1773bb0a56
Omotolani <torhlarny15@gmail.com>
2023-03-22 08:22:07 +0100

    Creat Funding and Transfer Opeerations

---
+++ src/main/kotlin/com/ai/doye/financeapp/dto/FundDTO.kt
@@ -0,0 +1,6 @@
+package com.ai.doye.financeapp.dto
+
+data class FundDTO(
+        var accountNo: String,
+        var amount: Double
+)
--- src/main/kotlin/com/ai/doye/financeapp/service/AccountService.kt
+++ src/main/kotlin/com/ai/doye/financeapp/service/AccountService.kt
@@ -1,5 +1,8 @@
 package com.ai.doye.financeapp.service
 
+import com.ai.doye.financeapp.dto.FundDTO
+import com.ai.doye.financeapp.dto.ResponseDTO
+import com.ai.doye.financeapp.dto.TransferDTO
 import com.ai.doye.financeapp.model.Account
 import com.ai.doye.financeapp.model.User
 import com.ai.doye.financeapp.repository.AccountRepository
@@ -29,7 +32,44 @@ class AccountService(
         return false
     }
 
+    fun fundAccount(fundDTO: FundDTO): ResponseDTO {
+        return try {
+            val account = accountRepository.findByAccountNo(fundDTO.accountNo)
+
+            account.balance += fundDTO.amount
+            accountRepository.save(account)
+
+            ResponseDTO("00","Account funded successfully")
+        } catch (e: Exception) {
+            logger.info("Exception occurred in [fundAccount] method")
+            ResponseDTO("XX", "Failed Funding Operation")
+        }
+    }
+
+    fun transfer(transferDTO: TransferDTO, accountNo: String): ResponseDTO {
+        return try {
+            val senderAccount = accountRepository.findByAccountNo(accountNo)
+            val recipientAccount = accountRepository.findByAccountNo(transferDTO.recipientAccountNo)
+
+            if(transferDTO.amount > senderAccount.balance) {
+                ResponseDTO("98", "Insufficient Balance to perform transfer")
+            }
+
+            senderAccount.balance -= transferDTO.amount
+            recipientAccount.balance += transferDTO.amount
+
+            accountRepository.save(senderAccount)
+            accountRepository.save(recipientAccount)
+
+            ResponseDTO("00","Transfer to ${recipientAccount.user?.fullName()} funded successfully")
+        } catch (e: Exception) {
+            logger.info("Exception occurred in [fundAccount] method")
+            ResponseDTO("XX", "Failed Transfer Operation")
+        }
+    }
+
     fun generateAccountNo(): String {
-        return format("%s", System.currentTimeMillis())
+        val currentTimeSeconds = System.currentTimeMillis() / 1000
+        return format("%s", currentTimeSeconds)
     }
 }
